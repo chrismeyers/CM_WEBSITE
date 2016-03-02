1669873c9a3d36fbf002c4063dac5751d9e40b76
chrismeyers <cm.02.93@gmail.com>
2016-03-01 20:55:43 -0500

    updated builds.

--- public_html/v4/index.php
+++ public_html/v4/index.php
@@ -135,6 +135,7 @@
                             <b>Specifications</b>
                             <ul>
                                 <li><u>Processor:</u> Intel Core i7 3770k &#64; 4.2GHz</li>
+                                <li><u>Cooling:</u> Cooler Master Hyper 212 Plus</li>
                                 <li><u>Mobo:</u> Asus P8Z77-V LK</li>
                                 <li><u>Memory:</u> 16GB Corsair Vengeance 1600MHz</li> 
                                 <li><u>HDD:</u> WD6400AAKS 640GB &#64; 7200rpm, ST31000524AS 1TB &#64; 7200rpm</li>
@@ -165,6 +166,7 @@
                             <b>Specifications</b>
                             <ul>
                                 <li><u>Processor:</u> Intel Core i7 4770k &#64; 3.5GHz</li>
+                                <li><u>Cooling:</u> Cooler Master Hyper 212 EVO</li>
                                 <li><u>Mobo:</u> MSI H87-G43</li>
                                 <li><u>Memory:</u> 16GB G.SKILL Ripjaws X Series 1600MHz</li>
                                 <li><u>HDD:</u> ST1000DM003 1TB &#64; 7200rpm</li>
@@ -183,8 +185,8 @@
                     <h3>February 2016 - Built for myself</h3>
                     <div class="build-info">
                         <div class="build-pic">
-                            <a href="images/builds/myBuild2016.jpg" class="fancybox" title="My Computer 2016">
-                                <img src="images/builds/myBuild2016.jpg" 
+                            <a href="images/builds/myBuild2016_liquid.jpg" class="fancybox" title="My Computer 2016">
+                                <img src="images/builds/myBuild2016_liquid.jpg" 
                                      class="build-pic-img-port"
                                      alt="My computer 2016" 
                                      title="Click to enlarge">
@@ -195,6 +197,7 @@
                             <b>Specifications</b>
                             <ul>
                                 <li><u>Processor:</u> Intel Core i7 6700k &#64; 4.2GHz</li>
+                                <li><u>Cooling:</u> Corsair H100i v2 w/ Noctua NF-F12 120mm PWM Fans</li>
                                 <li><u>Mobo:</u> Asus Republic of Gamers Maximus VIII Hero Alpha</li>
                                 <li><u>Memory:</u> 32GB HyperX Fury 2666MHz</li>
                                 <li><u>HDD:</u> Western Digital Blue 1TB &#64; 7200rpm</li>
