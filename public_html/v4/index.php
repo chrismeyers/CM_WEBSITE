c52b083c26d3906b7becacbb0228e62115b2969c
chrismeyers <cm.02.93@gmail.com>
2016-03-05 19:35:48 -0500

    updated about section.

--- public_html/v4/index.php
+++ public_html/v4/index.php
@@ -43,7 +43,7 @@
                 <a class="fancytxt" href="http://www.rowan.edu/" target="_blank">Rowan University</a>
                 in Glassboro, NJ and earned a Bachelor of Science in Computer Science. 
                 Currently, I am employed as a Software Engineer at 
-                <a class="fancytxt" href="http://asrcfederal.com/mse" target="_blank">Mission Solutions Engineering</a>.
+                <a class="fancytxt" href="http://asrcfederal.com/mse" target="_blank">ASRC Federal Mission Solutions</a>.
                 Some of my professional interests include: Software Development, Web Development and Computer Networking.
                 Over the past few years, I have picked up experience with the following programming languages and technologies:
                 
@@ -51,7 +51,7 @@
                     <li class="li-filled">Java, C++, C#, PHP, HTML, CSS, Javascript, MySQL, C, Visual Basic, Python, Perl, 
                         MIPS assembly, Scheme (LISP dialect), Ada, Lua.</li>
                 </ul>
-                
+
                 <p>      
                 Aside from programming, I have interests in Computer Aided Drafting (CAD) and 3D Modeling.  I have 4+ years of
                 experience in the following programs:
@@ -62,9 +62,14 @@
                 </ul>
 
                 <p>
-                During my senior year of High School, I competed in the NJ Technology Student Association annual 
+                During my senior year of High School, I competed in the NJ Technology Student Association (TSA) annual 
                 competition.  I placed 3rd in the 'Computer-Aided Design (CAD), 3D, Engineering' category
                 (<a class="fancytxt" href="http://njtsa.pages.tcnj.edu/files/2011/04/2011-HS-Event-Results-PPT1.pdf#page=7" target="_blank">Event Results</a>).
+                </p>
+
+                <p>
+                I enjoy building custom computers as a hobby and have done so for myself and others over the past few years.  My builds can be seen 
+                <a href='#builds' class="fancytxt" onclick="showSection('my-builds')">here</a>.
                 </p>
 
                 <p>
