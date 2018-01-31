324ce611a6c15f27fec0a2af6d1e6591e08f7994
Chris Meyers <cm.02.93@gmail.com>
2018-01-30 22:29:38 -0500

    updated about page.

--- public_html/v6/sections/about.php
+++ public_html/v6/sections/about.php
@@ -2,16 +2,29 @@
     <div class="section-header section-header-size">About</div>
 
     <div class="content-text">
-        <span>Hey, my name is <b>Chris Meyers</b>. This website is dedicated to letting you know who I am and the technology skills that I possess. Here's a quick list of things you should know about me:</span>
+        <span>My name is Chris Meyers and I am a Software Engineer and technology enthusiast. Here's a quick list of things you should know about me:</span>
 
         <ul>
             <li>I studied Computer Science at <a class="fancytxt" href="http://www.rowan.edu/" target="_blank">Rowan University</a> in Glassboro, NJ and earned a Bachelor of Science in Computer Science.</li>
             <li>Currently, I am employed as a Software Engineer at <a class="fancytxt" href="https://www.mcgstrategic.com" target="_blank">MCG Strategic</a>.</li>
-            <li>Some of my professional interests include: Software Development, Web Development, and Computer Networking.</li>
-            <li>My hobbies include: photography, reading,  <a href='#builds' class="fancytxt">building custom computers</a>, and Computer Aided Drafting (CAD).</li>
-            <li>I am the owner of this <a data-fancybox="gallery_clark" href="images/v6/clark/DSC_1316-6.jpg?v=4" title="Clark the Corgi" class="fancytxt no-outline">majestic beast</a>.</li>
+            <li>Some of my professional interests include:</li>
+            <ul>
+                <li>Web Development</li>
+                <li>Software Development</li>
+                <li>Computer Networking</li>
+            </ul>
+            <li>My hobbies include:</li>
+            <ul>
+                <li>Photography</li>
+                <li>Reading</li>
+                <li><a href='#builds' class="fancytxt">Building custom computers</a></li>
+                <li>Playing video games</li>
+            </ul>
+            <li>I am the owner of this <a data-fancybox="gallery_clark" href="images/v6/clark/DSC_1564-6.jpg" title="Clark the Corgi" class="fancytxt no-outline">majestic beast</a>.</li>
         </ul>
 
-        <span>Feel free to visit my social media or contact me on the <a class="fancytxt" href="#contact">contact page</a>.</span>
+        <span>I have spent my entire professional career striving to write clean, modular, and maintainable code. I am always up for the challenge of learning new
+        languages and frameworks. If a language or framework that I'm unfamiliar with is the best choice for a project, I have no hesitation in breaking away from
+        my comfort zones to explore new options. My primary goal is to create innovative and usable applications that solve problems.</span>
     </div>
 </div>
