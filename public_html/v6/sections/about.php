d0e4980ac3ad7885e6099e9ed8d7fcfec431ab2b
Chris Meyers <cm.02.93@gmail.com>
2018-05-05 13:57:00 -0400

    updated about section.

--- public_html/v6/sections/about.php
+++ public_html/v6/sections/about.php
@@ -7,26 +7,38 @@
         <ul>
             <li>I studied Computer Science at <a class="fancytxt" href="http://www.rowan.edu/" target="_blank">Rowan University</a> in Glassboro, NJ and earned a Bachelor of Science in Computer Science.</li>
             <li>Currently, I am employed as a Software Engineer at <a class="fancytxt" href="https://www.mcgstrategic.com" target="_blank">MCG Strategic</a>.</li>
-            <li>Some of my professional interests include:</li>
-            <ul>
-                <li>Web Development</li>
-                <li>Software Development</li>
-                <li>Computer Networking</li>
-            </ul>
-            <li>My hobbies include:</li>
-            <ul>
-                <li>Photography</li>
-                <li>Reading</li>
-                <li><a href="#builds" class="fancytxt">Building custom computers</a></li>
-                <li>Playing video games</li>
-            </ul>
+            <li>Some of my professional interests include:
+                <ul>
+                    <li>Web Development, Software Development, Computer Networking</li>
+                </ul>
+            </li>
+            <li>My hobbies include:
+                <ul>
+                    <li>Photography, reading, <a href="#builds" class="fancytxt">building custom computers</a>, playing video games</li>
+                </ul>
+            </li>
             <li>I am the owner of this <a data-fancybox="gallery_clark" href="images/v6/clark/DSC_1564-6.jpg" title="Clark the Corgi" class="fancytxt no-outline">majestic beast</a>.</li>
         </ul>
 
-        <span>I have spent my entire professional career striving to write clean, modular, and maintainable code. I am always up for the challenge of learning new
-        languages and frameworks. If a language or framework that I'm unfamiliar with is the best choice for a project, I have no hesitation in breaking away from
-        my comfort zones to explore new options. My primary goal is to create innovative and usable applications that solve problems.</span>
+        <p>I've gained experience with the following programming languages and frameworks:</p>
 
-        <p>I am currently available for freelance work. <a href="#contact" class="fancytxt">Send me a message</a> if you have any questions, comments, or would like to work together on a project.</p>
+        <ul>
+            <li>Desktop and CLI applications:
+                <ul>
+                    <li>C++, Python, Java, C#</li>
+                </ul>
+            </li>
+            <li>Websites, Web Apps, and APIs:
+                <ul>
+                    <li>LAMP stack (Linux, Apache, MySQL, PHP), Flask (Python web framework), JavaScript (jQuery, AngularJS, Node.js, React)</li>
+                </ul>
+            </li>
+        </ul>
+
+        <span>I have spent my entire professional programming career striving to write clean, modular, and maintainable code. I am always up for the challenge of
+        learning new languages and frameworks. If a language or framework that I'm unfamiliar with is the best choice for a project, I have no hesitation in
+        breaking away from my comfort zones to explore new options. My primary goal is to create innovative and usable applications that solve problems.</span>
+
+        <p><a href="#contact" class="fancytxt">Send me a message</a> if you have any questions, comments, or would like to work together on a project.</p>
     </div>
 </div>
